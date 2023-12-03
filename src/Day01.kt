3f1df4117e166e5a2841640017ba0770666074af
Niklas Rodenbach <nrodenbach@progeek.de>
2023-12-03 21:27:28 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val stringToNumber = mapOf(
+        "one" to "o1e",
+        "two" to "t2o",
+        "three" to "t3e",
+        "four" to "f4r",
+        "five" to "f5e",
+        "six" to "s6x",
+        "seven" to "s7n",
+        "eight" to "e8t",
+        "nine" to "n9e",
+    )
+
+    fun calibrationValue(input: String): Int =
+        "${input.first { it.isDigit() }}${input.last { it.isDigit() }}".toInt()
+
+    fun part1(input: List<String>): Int =
+        input.sumOf(::calibrationValue)
+
+    fun part2(input: List<String>): Int = input.sumOf { line ->
+        var tempString = line
+        for (entry in stringToNumber) {
+            tempString = tempString.replace(entry.key, entry.value)
+        }
+
+        calibrationValue(tempString)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = readInput("Day01/Day01_test")
+    val testInput2 = readInput("Day01/Day01_test2")
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    check(part2(testInput2) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01/Day01_input")
     part1(input).println()
     part2(input).println()
 }
